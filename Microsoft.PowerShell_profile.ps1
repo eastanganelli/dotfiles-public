b9f9bb42951781b06e3225815c36c76b1f5389a7
eastanganelli <ezequielstanganelli@gmail.com>
2023-03-14 11:30:09 -0300

    version 2

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,39 +1,13 @@
 ## Variables
-$DEV = $Env:DEV_TOOLS
+$DEV = $Env:WINAPPS
+$DEPTHAI = $Env:DEPTHAI
 
-## INSTALLS
-function Instaladores() {
-    Install-Module -Name Terminal-Icons -Repository PSGallery
-    Install-Module -Name z
-    Install-Module -Name PSFzf
-    Install-Module discordrpc -Scope CurrentUser
-}
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
 
 ## Init
 function Iniciadores() {
-    ## OH-MY-POSH
-    Import-Module posh-git
-    Import-Module oh-my-posh
 
-    ## PSReadLine
-    Set-PSREadLineOption -PredictionSource History
-
-    ## Terminal-Icons
-    Import-Module -Name Terminal-Icons
-    ##Get-Item ./README.md
-    ##Get-ChildItem
-    ##Get-ChildItem | Format-List
-    ##Get-ChildItem | Format-Wide
-
-    ## Fzf
-    Import-Module PSFzf
-    Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
-
-    ## Set theme
-    Set-PoshPrompt -Theme cinnomon
-
-    ## Load prompt configs
-    oh-my-posh --init --shell pwsh --config "~/.cinnomon.omp.json" | Invoke-Expression
 }
 
 ## Ultilities (Optional)
@@ -50,16 +24,12 @@ function Cleaning {
    $Ui.CursorPosition = $Coordinates
 }
 
-## Discord Presence
-$params = @{
-    Details      = "Version $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
-    State        = (Split-Path -Path $pwd -Leaf)
-    Start        = "Now"
-    UpdateScript = {
-        # show the directory you are in. you can do anything here.
-        ## Update-DSRichPresence -State (Split-Path -Path $pwd -Leaf)
-        Update-DSRichPresence -State (State)
-    }
+function oakcam {
+    python3  $DEPTHAI"\\depthai_demo.py"  --app=uvc
+}
+
+function ctt {
+    irm christitus.com/win | iex
 }
 
 ## Expressions
@@ -69,25 +39,20 @@ $params = @{
 Set-Alias g git
 Set-Alias ll ls
 Set-Alias grep findstr
-Set-Alias tig "C:\Program Files\Git\usr\bin\tig.exe"
-Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
-Set-Alias python ($DEV + "\python\python.exe")
-    
+Set-Alias tig  "C:\\Program Files\\Git\\usr\\bin\\tig.exe"
+Set-Alias less "C:\\Program Files\\Git\\usr\\bin\\less.exe"
+
 New-Alias -Name clr  -Value Cleaning
 
-Invoke-Expression Iniciadores
-
-# Start-DSClient @params
-
-Invoke-Expression Cleaning
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-
 if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
+  Import-Module "$ChocolateyProfile"
 }
 
+clr
